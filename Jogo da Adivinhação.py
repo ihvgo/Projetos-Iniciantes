5546596d3930215e8c3d92b1fe84e220b035f2ff
ivhugo_. <166242740+ihvgo@users.noreply.github.com>
2025-07-02 11:13:49 -0300

    Jogo da Adivinhação.py

--- Jogo da Adivinhação.py
+++ Jogo da Adivinhação.py
@@ -1,5 +1,5 @@
 import random
-n = random.randint(0, 5)
+n = random.randint(0, 5) #Faz o computador "Pensar" em um número
 escolha = int(input('Estou pensando em um número entre 0 e 5. Tente acertar qual número é: '))
 if escolha == n:
     print('Você acertou o número que eu pensei.')
